a0ccca49f09a1c9621cb0d8720b2c286cd8fc260
klifvegeta <klifvegeta@gmail.com>
2018-11-15 01:45:36 +0700

    Menambahkan PostgreSQL

--- web_app/app.py
+++ web_app/app.py
@@ -6,7 +6,17 @@ def create_app():
 
     @app.route('/')
     def index():
-        return render_template('index.html', TITLE='Flask-01')
+        import psycopg2
+
+        con = psycopg2.connect('dbname=flask01 user=devuser password=devpassword host=postgres')
+        cur = con.cursor()
+
+        cur.execute('select contents from page where id = 1')
+
+        contents = cur.fetchone() #mengembalikan  tuple. (0, 1, 2)
+        con.close()
+
+        return render_template('index.html', TITLE='Flask-01', CONTENT=contents[0])
 
     @app.route('/about')
     def about():
